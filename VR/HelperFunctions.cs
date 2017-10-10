9c92c7088cc8be13af71ceb64d795c57ca1949cd
alexOnorati <aonorati@mail.greenriver.edu>
2017-10-10 15:39:49 -0700

    Added helper functions

---
+++ VR/HelperFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Helper functions to help writing clean easy to read code.
+/// </summary>
+public static class HelperFunctions {
+
+    public static bool IsNull<T>(this T arg) {
+        return arg == null;
+    }
+
+    public static bool IsNotNull<T>(this T arg) {
+        return arg != null;
+    }
+}
